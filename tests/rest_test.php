b1c42e7b3f77e0020485e7fd066ba676d0b404f5
Kruithne <kruithne@gmail.com>
2013-12-19 03:15:52 +0000

    Added a simple REST interface test.

---
+++ tests/rest_test.php
@@ -0,0 +1,14 @@
+<?php
+	require_once('../KrameWork/KrameSystem.php'); // We need this.
+
+	$system = new KrameSystem(KW_DEFAULT_FLAGS & ~KW_ENABLE_SESSIONS); // Create a system without sessions.
+
+	$value = REST::Get('test', FILTER_VALIDATE_EMAIL);
+
+	if ($value === null)
+		print('No test value was given, add &test=bob@nowhere.net to the end of this page URL.');
+	else if ($value === false)
+		print('The test value given was NOT a e-mail address.');
+	else
+		print('The test value given was a valid e-mail address!');
+?>
