4369b353ee10dbc24ef2b6eb1848fbe163b604f3
Kruithne <kruithne@gmail.com>
2013-12-20 23:37:26 +0000

    Implemented extra parameter to add strings multiple times in one call.

--- KrameWork/StringBuilder.php
+++ KrameWork/StringBuilder.php
@@ -5,11 +5,12 @@
 		 * Appends a string to the end of the string builder.
 		 *
 		 * @param string $string A string to append.
+		 * @param int $times How many times the string should be appended.
 		 * @return StringBuilder $this The string builder instance.
 		 */
-		public function append($string)
+		public function append($string, $times = 1)
 		{
-			$this->string .= $string;
+			$this->string .= str_repeat($string, $times);
 			return $this;
 		}
 
@@ -17,11 +18,12 @@
 		 * Prepends a string to the start of the string builder.
 		 *
 		 * @param string $string The string to prepend.
+		 * * @param int $times How many times the string should be prepended.
 		 * @return StringBuilder $this The string builder instance.
 		 */
-		public function prepend($string)
+		public function prepend($string, $times = 1)
 		{
-			$this->string = $string . $this->string;
+			$this->string = $string . str_repeat($this->string, $times);
 			return $this;
 		}
 
