448472230c5eb6f675b07d8ab803b29b2f3224c4
Kruithne <kruithne@gmail.com>
2013-12-21 01:00:27 +0000

    Corrected import path for the basic_example.

--- examples/basic_example/basic_app.php
+++ examples/basic_example/basic_app.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('../KrameWork/KrameSystem.php'); // We need this.
+	require_once('../../KrameWork/KrameSystem.php'); // We need this.
 
 	$system = new KrameSystem(); // Create a system object, how sweet!
 	$system->addAutoLoadPath(getcwd()); // Auto-load from the current directory.
