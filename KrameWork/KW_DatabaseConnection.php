82503e31d8645c4756ccb3346db31429fd147581
Kruithne <kruithne@gmail.com>
2013-12-22 00:06:31 +0000

    Implemented basic database support.

--- KrameWork/KW_DatabaseConnection.php
+++ KrameWork/KW_DatabaseConnection.php
@@ -1,21 +1,45 @@
 <?php
 	class KW_DatabaseConnection
 	{
+		/**
+		 * Constructs a database connection wrapper.
+		 *
+		 * @param string $dsn DB DSN (PDO format)
+		 * @param string|null $username Username to connect with. Set to NULL if N/A.
+		 * @param string|null $password Password to connect with. Set to NULL if N/A.
+		 */
 		public function __construct($dsn, $username, $password)
 		{
 			$this->connection = new PDO($dsn, $username, $password);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 
-		public function query($sql)
+		/**
+		 * Returns a database statement.
+		 *
+		 * @param string $sql An SQL query for this statement.
+		 * @return KW_DatabaseStatement A database statement.
+		 */
+		public function prepareStatement($sql)
 		{
-			return $this->connection->query($sql);
+			return new KW_DatabaseStatement($sql, $this->connection);
 		}
 
-		public function prepare($sql)
+		/**
+		 * Execute an SQL query.
+		 *
+		 * @param string $sql An SQL query string.
+		 * @return int Amount of rows effected by the execution of this query.
+		 */
+		public function executeQuery($sql)
 		{
-			return $this->connection->prepare($sql);
+			return $this->connection->exec($sql);
 		}
 
+		/**
+		 * @param string $table The name of the table to check.
+		 * @return string ID of the last inserted row.
+		 */
 		public function getLastInsertID($table)
 		{
 			return $this->connection->lastInsertId($table);
