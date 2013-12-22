f474468e5970ec1050b1d3ef2a0c5f7e23cc94ce
Kruithne <kruithne@gmail.com>
2013-12-22 00:46:00 +0000

    Updated the database example to show a bit more code.

--- examples/database_example/database_example.php
+++ examples/database_example/database_example.php
@@ -6,13 +6,32 @@
 	$system->getErrorHandler()->addEmailOutputRecipient('kruithne@gmail.com');
 
 	// Start a new database connection.
-	$database = new KW_DatabaseConnection('sqlite:test_database.sq3', null, null);
+	$db = new KW_DatabaseConnection('sqlite:test_database.sq3', null, null);
 
-	// Create an SQL statement and execute it.
-	$statement = $database->prepareStatement("SELECT test_column FROM test_table");
-	$statement->execute();
+	// Simple function to make repeating the query easy!
+	function getTestRows()
+	{
+		global $db;
+
+		// Create an SQL statement and execute it.
+		$statement = $db->prepare("SELECT test_column FROM test_table");
+		$statement->execute();
+
+		return $statement->getRows();
+	}
+
+
+	// Get every row and reverse the value.
+	foreach (getTestRows() as $row)
+	{
+		$row->test_column = $row->test_column == 'Hello, world!' ? 'World, hello!' : 'Hello, world!';
+		$update = $db->prepare('UPDATE test_table SET test_column = :test_column');
+		$update->copyValuesFromRow($row)->execute();
+	}
 
 	// Loop every row we have and output the value of test_column.
-	foreach ($statement->getRows() as $row)
+	foreach (getTestRows() as $row)
 		echo $row->test_column . HTML_EOL;
+
+	// The output of this script will revert between 'Hello, world!' and 'World, hello!' each time it's run.
 ?>
