88d13c1c9328a70dacc94a5cd14667c6cb41c156
Morten Nilsen <morten@runsafe.no>
2015-09-30 00:38:59 +0200

    Implemented a JSON service base class

---
+++ KrameWork/KW_JSONService.php
@@ -0,0 +1,24 @@
+<?php
+	class KW_JSONService
+	{
+		public function __construct()
+		{
+			$this->request = json_decode(file_get_contents('php://input'));
+			$this->response = (object)array();
+		}
+
+		public function __tostring()
+		{
+		}
+
+		public function respond()
+		{
+			header('Content-Type: application/json;charset=UTF-8');
+			echo json_encode($this->response);
+			die();
+		}
+
+		protected $request;
+		protected $response;
+	}
+?>
