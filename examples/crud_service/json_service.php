58e85edf6f6d1523c1d677c0bdfb42f0061b022b
Morten Nilsen <morten@runsafe.no>
2015-10-29 21:00:51 +0100

    Add sample CRUD Service, untested.

---
+++ examples/crud_service/json_service.php
@@ -0,0 +1,15 @@
+<?php
+	// This is a JSON based service endpoint
+	require_once('../../KrameWork/KrameSystem.php'); // We need this.
+
+	$system = new KrameSystem(KW_DEFAULT_FLAGS & ~KW_ENABLE_SESSIONS); // Create a system without sessions.
+	$system->addAutoLoadPath(getcwd()); // Auto-load from the current directory.
+
+	$system->addComponent(
+		new KW_DatabaseConnection('pgsql:dbname=test;host=localhost', 'test', 'password')
+	);
+	$system->addComponent('KW_SchemaManager');
+	$system->addComponent('SimpleService');
+
+	$system->getComponent('SimpleService'); // Expose service to clients
+?>
