34e5ef142c00c6ba1ab19ac029ab20101501e2a3
Kruithne <kruithne@gmail.com>
2016-01-06 14:25:34 +0000

    Base class to glue JSONService and DataContainer functionality together

---
+++ KrameWork/KW_JSONObject.php
@@ -0,0 +1,45 @@
+<?php
+	abstract class KW_JSONObject extends KW_JSONService
+	{
+		/**
+		 * Calls __get on the underlying data container.
+		 *
+		 * @param $name string
+		 * @return mixed
+		 * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+		 */
+		function __get($name)
+		{
+			return $this->data->__get($name);
+		}
+
+		/**
+		 * Calls __set on the underlying data container.
+		 *
+		 * @param $name string
+		 * @param $value mixed
+		 * @return void
+		 * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+		 */
+		function __set($name, $value)
+		{
+			$this->data->__set($name, $value);
+		}
+
+		/**
+		 * Calls _invoke on the underlying data container.
+		 *
+		 * @return mixed
+		 * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.invoke
+		 */
+		function __invoke($arr)
+		{
+			$this->data->__invoke($arr);
+		}
+
+		/**
+		 * @var KW_DataContainer
+		 */
+		protected $data;
+	}
+?>
