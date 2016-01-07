eb82511673b727059a4671fc9d0e92e2eda9d3b8
Kruithne <kruithne@gmail.com>
2016-01-07 16:51:13 +0000

    More elegant approach to Path::Clean

--- KrameWork/Path.php
+++ KrameWork/Path.php
@@ -18,22 +18,7 @@
 		 */
 		public static function Clean($path)
 		{
-			$path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-			$parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-			$absolutes = array();
-
-			foreach ($parts as $part)
-			{
-				if ('.' == $part)
-					continue;
-
-				if ('..' == $part)
-					array_pop($absolutes);
-				else
-					$absolutes[] = $part;
-			}
-
-			return implode(DIRECTORY_SEPARATOR, $absolutes);
+			return preg_replace("/[\\\/]+/", DIRECTORY_SEPARATOR, $path);
 		}
 	}
 ?>
