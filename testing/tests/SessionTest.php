4f65f8db167ee1665ec09a05eead6bf6427e6875
Kruithne <kruithne@gmail.com>
2016-01-09 14:10:18 +0000

    Basic tests for the static Session class

---
+++ testing/tests/SessionTest.php
@@ -0,0 +1,37 @@
+<?php
+	require_once("/home/travis/build/Kruithne/KrameWork/testing/resources/default_bootstrap.php");
+
+	class SessionTest extends PHPUnit_Framework_TestCase
+	{
+		/**
+		 * Check that setting/getting with our static Session class works.
+		 */
+		public function testSessionGetSet()
+		{
+			Session::Set("testSessionGetSet", 42);
+			$rt = Session::Get("testSessionGetSet");
+
+			$this->assertEquals(42, $rt);
+		}
+
+		/**
+		 * Confirm that trying to get a value that does not exist returns NULL.
+		 */
+		public function testSessionInvalid()
+		{
+			$rt = Session::Get("someValueWeDidNotSet");
+			$this->assertEquals(null, $rt);
+		}
+
+		/**
+		 * Values deleted using our Session interface should return NULL.
+		 */
+		public function testSessionDelete()
+		{
+			Session::Set("testSessionDelete", 1337);
+			Session::Delete("testSessionDelete");
+
+			$this->assertEquals(null, "testSessionDelete");
+		}
+	}
+?>
