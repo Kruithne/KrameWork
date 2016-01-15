38a0d4c3612c44736dc9c478212e2c2d3a227327
Morten Nilsen <morten@runsafe.no>
2016-01-15 23:18:50 +0100

    Add more tests to CRUD

--- testing/resources/MockCRUD.php
+++ testing/resources/MockCRUD.php
@@ -1,14 +1,35 @@
 <?php
 	class MockCRUD extends KW_CRUD
 	{
-		public function getKey() { return 'id'; }
-		public function hasAutoKey() { return true; }
-		public function getValues() { return array('value'); }
-		public function getName() { return '__mock__'; }
-		public function getVersion() { return 1; }
-		public function getQueries()
+		public function __construct(
+			ISchemaManager $schema,
+			$key = 'id', $auto = true,
+			$values = array('value'),
+			$name = '__mock__', $version = 1,
+			$queries = array(1 => array('CREATE __mock__'))
+		)
 		{
-			return array(1 => array('CREATE __mock__'));
+			$this->key = $key;
+			$this->auto = $auto;
+			$this->values = $values;
+			$this->name = $name;
+			$this->version = $version;
+			$this->queries = $queries;
+			parent::__construct($schema);
 		}
+
+		public function getKey() { return $this->key; }
+		public function hasAutoKey() { return $this->auto; }
+		public function getValues() { return $this->values; }
+		public function getName() { return $this->name; }
+		public function getVersion() { return $this->version; }
+		public function getQueries() { return $this->queries; }
+
+		private $key;
+		private $auto;
+		private $values;
+		private $name;
+		private $version;
+		private $queries;
 	}
 ?>
--- testing/tests/CRUDTest.php
+++ testing/tests/CRUDTest.php
@@ -6,7 +6,7 @@
 		/**
 		 * Test that generated insert query is correct and gets executed
 		 */
-		public function testCreateOperation()
+		public function testAutoIDCreateOperation()
 		{
 			$db = MockDatabaseConnection::Get();
 			$manager = new MockSchemaManager($db);
@@ -20,7 +20,7 @@
 		/**
 		 * Test that generated select query is correct and gets executed
 		 */
-		public function testReadAllOperation()
+		public function testAutoIDReadAllOperation()
 		{
 			$db = MockDatabaseConnection::Get();
 			$manager = new MockSchemaManager($db);
@@ -34,7 +34,7 @@
 		/**
 		 * Test that generated select query is correct and gets executed
 		 */
-		public function testReadOneOperation()
+		public function testAutoIDReadOneOperation()
 		{
 			$db = MockDatabaseConnection::Get();
 			$manager = new MockSchemaManager($db);
@@ -48,7 +48,7 @@
 		/**
 		 * Test that generated update query is correct and gets executed
 		 */
-		public function testUpdateOperation()
+		public function testAutoIDUpdateOperation()
 		{
 			$db = MockDatabaseConnection::Get();
 			$manager = new MockSchemaManager($db);
@@ -62,7 +62,7 @@
 		/**
 		 * Test that generated delete query is correct and gets executed
 		 */
-		public function testDeleteOperation()
+		public function testAutoIDDeleteOperation()
 		{
 			$db = MockDatabaseConnection::Get();
 			$manager = new MockSchemaManager($db);
@@ -71,6 +71,132 @@
 			$result = $crud->delete((object)array('id' => 1));
 			$sql = $db->end();
 			$this->assertEquals('DELETE FROM __mock__ WHERE id = :id', $sql, 'Delete query mismatch');
+		}
+
+		/**
+		 * Test that generated insert query is correct and gets executed
+		 */
+		public function testCompositeCreateOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->create((object)array('a' => 1, 'b' => 2, 'value' => '{test}'));
+			$sql = $db->end();
+			$this->assertEquals('INSERT INTO __mock__ (a, b, value) VALUES (:a, :b, :value)', $sql, 'Insert query mismatch');
+		}
+
+		/**
+		 * Test that generated select query is correct and gets executed
+		 */
+		public function testCompositeReadAllOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->read();
+			$sql = $db->end();
+			$this->assertEquals('SELECT * FROM __mock__', $sql, 'Select all query mismatch');
+		}
+
+		/**
+		 * Test that generated select query is correct and gets executed
+		 */
+		public function testCompositeReadSubsetOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->read(array('a' => 1, 'b' => '*'));
+			$sql = $db->end();
+			$this->assertEquals('SELECT * FROM __mock__ WHERE ??', $sql, 'Select all query mismatch');
+		}
+
+		/**
+		 * Test that generated select query is correct and gets executed
+		 */
+		public function testCompositeReadOneOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->read(array('a' => 1, 'b' => 2));
+			$sql = $db->end();
+			$this->assertEquals('SELECT * FROM __mock__ WHERE a = :a AND b = :b', $sql, 'Select one query mismatch');
+		}
+
+		/**
+		 * Test that generated update query is correct and gets executed
+		 */
+		public function testCompositeUpdateOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->update((object)array('a' => 1, 'b' => 2, 'value' => 'mock'));
+			$sql = $db->end();
+			$this->assertEquals('UPDATE __mock__ SET value = :value WHERE a = :a AND b = :b', $sql, 'Update query mismatch');
+		}
+
+		/**
+		 * Test that generated delete query is correct and gets executed
+		 */
+		public function testCompositeDeleteOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, array('a','b'), false);
+			$db->begin();
+			$result = $crud->delete((object)array('a' => 1, 'b' => 2));
+			$sql = $db->end();
+			$this->assertEquals('DELETE FROM __mock__ WHERE a = :a AND b = :b', $sql, 'Delete query mismatch');
+		}
+
+		/**
+		 * Test that generated insert query is correct and gets executed
+		 */
+		public function testCreateOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, null, false);
+			$db->begin();
+			$result = $crud->create((object)array('value' => '{test}'));
+			$sql = $db->end();
+			$this->assertEquals('INSERT INTO __mock__ (value) VALUES (:value)', $sql, 'Insert query mismatch');
+		}
+
+		/**
+		 * Test that generated select query is correct and gets executed
+		 */
+		public function testReadAllOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, null, false);
+			$db->begin();
+			$result = $crud->read();
+			$sql = $db->end();
+			$this->assertEquals('SELECT * FROM __mock__', $sql, 'Select all query mismatch');
+		}
+
+		/**
+		 * Test that generated delete query is correct and gets executed
+		 */
+		public function testDeleteOperation()
+		{
+			$db = MockDatabaseConnection::Get();
+			$manager = new MockSchemaManager($db);
+			$crud = new MockCRUD($manager, null. false);
+			$db->begin();
+			$result = $crud->delete((object)array('value' => 'mock'));
+			$sql = $db->end();
+			$this->assertEquals('DELETE FROM __mock__ WHERE value = :value', $sql, 'Delete query mismatch');
 		}
 	}
 ?>
