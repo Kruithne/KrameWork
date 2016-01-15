5564961acca2344c6116f7a04f2f08aabc5180fa
Morten Nilsen <morten@runsafe.no>
2016-01-15 21:46:03 +0100

    Autoload testing resources

--- testing/resources/default_bootstrap.php
+++ testing/resources/default_bootstrap.php
@@ -4,6 +4,7 @@
 	require_once(TRAVIS_EXEC_PATH . "KrameWork/KrameWork/KrameSystem.php");
 	date_default_timezone_set("Europe/London");
 
+	KW_ClassLoader::addClassPath(TRAVIS_EXEC_PATH . "KrameWork/resources/");
 	KW_ClassLoader::addClassPath(TRAVIS_EXEC_PATH . "KrameWork/testing/");
 
 	$sys = new KrameSystem();
