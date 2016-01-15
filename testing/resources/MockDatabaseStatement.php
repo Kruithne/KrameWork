cc2f944d3da6f8e175e633bb43f789c916e4b0a3
Morten Nilsen <morten@runsafe.no>
2016-01-15 21:43:25 +0100

    Write the first database access test

---
+++ testing/resources/MockDatabaseStatement.php
@@ -0,0 +1,57 @@
+<?php
+	class MockDatabaseStatement implements IDatabaseStatement
+	{
+		public function getQueryString()
+		{
+			return $this->sql;
+		}
+
+		public function setValue($key, $value)
+		{
+			$this->data[$key] = $value;
+		}
+
+		public function setType($key, $type)
+		{
+			$this->type[$key] = $type;
+		}
+
+		public function __set($key, $value)
+		{
+			$this->data[$key] = $value;
+		}
+
+		public function copyValuesFromRow($row, $prependChar = ':')
+		{
+			return null;
+		}
+
+		public function execute()
+		{
+		}
+
+		public function getRows()
+		{
+			return array();
+		}
+
+		public function getFirstRow()
+		{
+			return false;
+		}
+
+		public function getRowCount()
+		{
+			return 0;
+		}
+
+		public function getErrorCode()
+		{
+			return '';
+		}
+
+		private $sql;
+		private $data = array();
+		private $type = array();
+	}
+?>
