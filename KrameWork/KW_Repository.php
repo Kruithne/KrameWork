e473a650b0850bceef0616f3fbed891d5d8ae253
Kruithne <kruithne@gmail.com>
2016-01-17 23:45:56 +0000

    Correct indenting in KW_Repository

--- KrameWork/KW_Repository.php
+++ KrameWork/KW_Repository.php
@@ -1,11 +1,10 @@
 <?php
-  abstract class KW_Repository extends KW_DataContainer implements IRepository
+	abstract class KW_Repository extends KW_DataContainer implements IRepository
 	{
 		public function setDB($db)
 		{
 			$this->db = $db;
 		}
-
 		protected $db;
 	}
 ?>
