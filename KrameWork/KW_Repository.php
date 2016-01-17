6be09464b6d64efe713c10bfe228ca255ac5b288
Kruithne <kruithne@gmail.com>
2016-01-17 23:46:28 +0000

    Introduce documentation to KW_Repository

--- KrameWork/KW_Repository.php
+++ KrameWork/KW_Repository.php
@@ -1,10 +1,18 @@
 <?php
 	abstract class KW_Repository extends KW_DataContainer implements IRepository
 	{
+		/**
+		 * Set the database connection for this repository.
+		 * @param IDatabaseConnection $db
+		 */
 		public function setDB($db)
 		{
 			$this->db = $db;
 		}
+
+		/**
+		 * @var IDatabaseConnection
+		 */
 		protected $db;
 	}
 ?>
