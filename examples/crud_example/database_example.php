15fa166983b07a296cab8b0c4e5280882b8c2306
Morten Nilsen <morten@runsafe.no>
2016-01-17 18:25:46 +0100

    Enhance example

--- examples/crud_example/database_example.php
+++ examples/crud_example/database_example.php
@@ -29,8 +29,28 @@
 
 	$schema = new KW_SchemaManager($db);
 	$test = new TestTable($schema);
+	// You should only call update when you have changed the schema, for best performance.
 	$schema->update();
+
+	// Create
+	$object = $test->create((object)array('value' => 'example data'));
 	$test->create((object)array('value' => sprintf('%s test data', date('r'))));
+
+	// Read
 	foreach($test->read() as $row)
 		printf("%s\n", $row->value);
+	$data = $test->read($object->rowid);
+	printf("Object %d : %s\n", $object->rowid, $data->value);
+
+	// Update
+	$object->value = 'Replaced data';
+	$test->update($object);
+	$data = $test->read($object->rowid);
+	printf("Object %d : %s\n", $object->rowid, $data->value);
+
+	// Delete
+	$test->delete($object);
+	// For some reason, sqlite still finds the row after it was deleted..
+	//	$data = $test->read($object->rowid);
+	//	printf($data ? "Object not deleted\n" : "Object deleted\n");
 ?>
