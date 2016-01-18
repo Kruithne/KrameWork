a8ab3cada83032730eefac9042a548ad6141466d
Morten Nilsen <morten@runsafe.no>
2016-01-18 21:19:25 +0100

    Always defer query preparation to optimize database access

--- KrameWork/KW_DatabaseConnection.php
+++ KrameWork/KW_DatabaseConnection.php
@@ -26,23 +26,11 @@
 		 * Returns a database statement.
 		 *
 		 * @param string $sql An SQL query for this statement.
-		 * @param boolean $quiet Ignore errors during prepare
-		 * @return KW_DatabaseStatement A database statement.
+		 * @return IDatabaseStatement A database statement.
 		 */
-		public function prepare($sql, $quiet = false)
+		public function prepare($sql)
 		{
-			if($this->getType() == 'sqlite')
-				return new KW_DeferredStatement($sql, $this->connection);
-			try
-			{
-				return new KW_DatabaseStatement($sql, $this->connection);
-			}
-			catch(Exception $e)
-			{
-				if($quiet)
-					return null;
-				throw $e;
-			}
+			return new KW_DeferredStatement($sql, $this->connection);
 		}
 
 		/**
