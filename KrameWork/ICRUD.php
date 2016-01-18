f66f5189f0b878e9ab3ba2ddadb39754662a0962
Kruithne <kruithne@gmail.com>
2016-01-18 19:26:06 +0000

    Template PHPDoc for ICrud

--- KrameWork/ICRUD.php
+++ KrameWork/ICRUD.php
@@ -1,16 +1,70 @@
 <?php
 	interface ICRUD
 	{
+		/**
+		 * DESCRIPTION
+		 * @return TYPE
+		 */
 		public function getKey();
+
+		/**
+		 * DESCRIPTION
+		 * @return TYPE
+		 */
 		public function hasAutoKey();
+
+		/**
+		 * DESCRIPTION
+		 * @return TYPE
+		 */
 		public function getValues();
+
+		/**
+		 * DESCRIPTION
+		 * @param TYPE $key
+		 * @return TYPE
+		 */
 		public function getKeyType($key);
+
+		/**
+		 * DESCRIPTION
+		 * @param TYPE $data
+		 * @return TYPE
+		 */
 		public function getNewObject($data);
+
+		/**
+		 * DESCRIPTION
+		 * @return TYPE
+		 */
 		public function prepare();
 
+		/**
+		 * DESCRIPTION
+		 * @param TYPE $object
+		 * @return TYPE
+		 */
 		public function create($object);
+
+		/**
+		 * DESCRIPTION
+		 * @param null|TYPE $key
+		 * @return TYPE
+		 */
 		public function read($key = null);
+
+		/**
+		 * DESCRIPTION
+		 * @param TYPE $object
+		 * @return TYPE
+		 */
 		public function update($object);
+
+		/**
+		 * DESCRIPTION
+		 * @param TYPE $object
+		 * @return TYPE
+		 */
 		public function delete($object);
 	}
 ?>
