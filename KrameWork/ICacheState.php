318699675b5e306eb715af733abd37c61e7dd9e4
Kruithne <kruithne@gmail.com>
2016-01-23 16:32:53 +0000

    Add missing phpDoc in ICacheState

--- KrameWork/ICacheState.php
+++ KrameWork/ICacheState.php
@@ -8,10 +8,12 @@
 		 */
 		public function clear($cacheKey);
 
-		/** 
+		/**
 		 * Called to get the timestamp for a cache key
 		 *
-		 * @param string $cacheKey A string identifying the cached object.
+		 * @param string $statement
+		 * @return int
+		 * @internal param string $cacheKey A string identifying the cached object.
 		 */
 		public function read($statement);
 	}
