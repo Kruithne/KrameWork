c605a13998eac18dd6d75cb9e8c8c6a60419faf9
Kruithne <kruithne@gmail.com>
2016-01-23 18:37:38 +0000

    Code adjustments @ KW_MultifactorService to match guidelines

--- KrameWork/KW_MultifactorService.php
+++ KrameWork/KW_MultifactorService.php
@@ -18,9 +18,11 @@
 		{
 			global $user;
 			$path = false;
-			if(isset($_SERVER['PATH_INFO']))
+
+			if (isset($_SERVER['PATH_INFO']))
 				$path = $_SERVER['PATH_INFO'];
-			if(!isset($_SESSION['userid']))
+
+			if (!isset($_SESSION['userid']))
 				return;
 
 			if($_SERVER['REQUEST_METHOD'] == 'POST')
@@ -28,15 +30,17 @@
 				switch($path)
 				{
 					case '/replace':
-				 		if($_SESSION['state'] != AUTH_OK && $_SESSION['state'] != AUTH_ERR_NOSECRET)
+				 		if ($_SESSION['state'] != AUTH_OK && $_SESSION['state'] != AUTH_ERR_NOSECRET)
 							return [];
-						if($user->secret)
+
+						if ($user->secret)
 						{
 							$result = $this->users->authenticate(
 								$user->username,
 								$request->passphrase
 							);
-							if($result != AUTH_OK)
+
+							if ($result != AUTH_OK)
 								return [];
 						}
 						$secret = $this->auth->createSecret();
@@ -45,25 +49,28 @@
 						return ['token' => $token];
 
 					case '/clone':
-				 		if($_SESSION['state'] != AUTH_OK)
+				 		if ($_SESSION['state'] != AUTH_OK)
 							return [];
+
 						$result = $this->users->authenticate(
 							$user->username,
 							$request->passphrase
 						);
-						if($result != AUTH_OK)
+
+						if ($result != AUTH_OK)
 							return [];
+
 						$token = $this->auth->getQRCodeGoogleUrl('runsafe-lab', $user->secret);
 						return ['token' => $token];
 
 					case '/verify':
-						if($user->lastcode == $request->code)
+						if ($user->lastcode == $request->code)
 							return ['result' => false, 'reason' => 'replay'];
 
-						if(isset($_SESSION['new_secret']) && $user->secret == null)
+						if (isset($_SESSION['new_secret']) && $user->secret == null)
 						{
 							$result = $this->auth->verifyCode($_SESSION['new_secret'], $request->code, 2);
-							if($result)
+							if ($result)
 							{
 								$this->users->setSecret($user->id, $_SESSION['new_secret']);
 								$user->secret = $_SESSION['new_secret'];
@@ -74,7 +81,9 @@
 						{
 							$result = $this->auth->verifyCode($user->secret, $request->code, 2);
 						}
+
 						$_SESSION['verified'] = $result;
+
 						if($result)
 							$_SESSION['state'] = $this->users->getState($user);
 						return ['result' => $result];
