7abad5ada8beb3d04d2575c2cf307472109aa261
Kruithne <kruithne@gmail.com>
2016-01-23 19:47:59 +0000

    Revert "Code adjustments @ KW_MetaTable to match guidelines"
    This reverts commit 347279186274c662b97a86b3ff5c5deb6e4fb2d2.

--- KrameWork/KW_MetaTable.php
+++ KrameWork/KW_MetaTable.php
@@ -8,42 +8,40 @@
 				case 'sqlite':
 					$this->exists = $this->db->prepare('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'_metatable\'');
 					$this->load = $this->db->prepare('SELECT * FROM _metatable');
-					$this->save = $this->db->prepare('INSERT OR IGNORE INTO _metatable ("table", "version") VALUES (:table, :version); UPDATE _metatable SET "version"=:version WHERE "table"=:table');
+					$this->save = $this->db->prepare('
+INSERT OR IGNORE INTO _metatable ("table", "version") VALUES (:table, :version);
+UPDATE _metatable SET "version"=:version WHERE "table"=:table');
 					break;
-
 				case 'pgsql':
 					$this->exists = $this->db->prepare('
-						SELECT c.relname
-						FROM   pg_catalog.pg_class c
-						JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-						WHERE  n.nspname = \'public\'
-						AND    c.relname = \'_metatable\'
-					');
-
+SELECT c.relname 
+FROM   pg_catalog.pg_class c
+JOIN   pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE  n.nspname = \'public\'
+AND    c.relname = \'_metatable\'
+');
 					$this->load = $this->db->prepare('SELECT * FROM _metatable');
 					$this->save = $this->db->prepare('UPDATE _metatable SET "version"=:version WHERE "table"=:table');
 					$this->create = $this->db->prepare('
-						INSERT INTO _metatable ("table","version")
-						SELECT _table, 0
-						FROM (
-							SELECT CAST(:table AS VARCHAR) AS _table
-						) AS i
-						LEFT JOIN _metatable ON (_metatable."table" = i._table)
-						WHERE _metatable."table" IS NULL
-					');
+INSERT INTO _metatable ("table","version")
+SELECT _table, 0
+FROM (
+	SELECT CAST(:table AS VARCHAR) AS _table
+) AS i
+LEFT JOIN _metatable ON (_metatable."table" = i._table)
+WHERE _metatable."table" IS NULL
+');
 					break;
-
 				case 'mysql':
 					$this->exists = $this->db->prepare('SHOW TABLES LIKE \'_metatable\'');
 					$this->load = $this->db->prepare('SELECT * FROM `_metatable`');
 					$this->save = $this->db->prepare('
-						INSERT INTO `_metatable` (`table`,`version`) VALUES (:table,:version)
-							ON DUPLICATE KEY UPDATE `version`=VALUES(`version`)
-					');
+INSERT INTO `_metatable` (`table`,`version`) VALUES (:table,:version)
+	ON DUPLICATE KEY UPDATE `version`=VALUES(`version`)
+');
 					break;
-
 				default:
-					trigger_error('The database driver "' . $this->db->getType() . '" is not yet supported by SchemaManager, sorry!', E_USER_ERROR);
+					trigger_error('The database driver "'.$this->db->getType().'" is not yet supported by SchemaManager, sorry!', E_USER_ERROR);
 			}
 		}
 
@@ -65,27 +63,25 @@
 					return array(
 						1 => array('CREATE TABLE _metatable ("table" TEXT PRIMARY KEY, "version" INT)')
 					);
-
 				case 'pgsql':
 					return array(
 						1 => array('
-							CREATE TABLE _metatable (
-								"table" VARCHAR(50),
-								"version" INTEGER,
-								PRIMARY KEY("table")
-							)'
+CREATE TABLE _metatable (
+	"table" VARCHAR(50),
+	"version" INTEGER,
+	PRIMARY KEY("table")
+)'
 						)
 					);
 					break;
-
 				default:
 					return array(
 						1 => array('
-							CREATE TABLE `_metatable` (
-								`table` VARCHAR(50),
-								`version` INTEGER,
-								PRIMARY KEY(`table`)
-							)'
+CREATE TABLE `_metatable` (
+	`table` VARCHAR(50),
+	`version` INTEGER,
+	PRIMARY KEY(`table`)
+)'
 						)
 					);
 			}
