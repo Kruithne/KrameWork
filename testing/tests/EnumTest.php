772b7d20e1398099dc17b9a7d5df49990983413a
Kruithne <kruithne@gmail.com>
2016-01-24 20:37:07 +0000

    Helpful assertion messages for enum testing

--- testing/tests/EnumTest.php
+++ testing/tests/EnumTest.php
@@ -9,16 +9,16 @@
 		public function testBasicEnumFunctions()
 		{
 			$enum = new MockEnum();
-			$this->assertEquals(42, $enum->valueOf('test_1'));
-			$this->assertEquals(1337, $enum->valueOf('test_2'));
+			$this->assertEquals(42, $enum->valueOf('test_1'), 'Enum value "test_1" does not equal 42.');
+			$this->assertEquals(1337, $enum->valueOf('test_2'), 'Enum value "test_2" does not equal 1337.');
 
 			// Case sensitivity.
-			$this->assertEquals(42, $enum->valueOf('TEST_1', true));
-			$this->assertEquals(null, $enum->valueOf('test_1', true));
+			$this->assertEquals(42, $enum->valueOf('TEST_1', true), 'Enum value "TEST_1" does not equal 42 (case-sensitive).');
+			$this->assertEquals(null, $enum->valueOf('test_1', true), 'Enum value "test_1" does not equal null (case-sensitive).');
 
 			// Missing keys.
-			$this->assertEquals(null, $enum->valueOf('missingKey', true));
-			$this->assertEquals(null, $enum->valueOf('missingKey', false));
+			$this->assertEquals(null, $enum->valueOf('missingKey', true), 'Enum value "missingKey" does not equal null (case-sensitive).');
+			$this->assertEquals(null, $enum->valueOf('missingKey', false), 'Enum value "missingKey" does not equal null.');
 		}
 	}
 ?>
