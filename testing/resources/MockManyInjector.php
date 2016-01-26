5701f9b1e64ea50d156b491c241ca8974086b2a1
Morten Nilsen <morten@runsafe.no>
2016-01-26 02:01:05 +0100

    ...

--- testing/resources/MockManyInjector.php
+++ testing/resources/MockManyInjector.php
@@ -1,8 +1,16 @@
 <?php
 	class MockManyInjector implements IManyInject
 	{
+		public function __construct($data)
+		{
+			$this->data = $data;
+		}
+
 		public function getComponents(string $type)
 		{
+			return $this->data;
 		}
+
+		private $data;
 	}
 ?>
