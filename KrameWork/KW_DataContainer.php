e8db5c9e440e5eb2af87f523bcb7a05fc70c5334
Morten Nilsen <morten@runsafe.no>
2016-03-29 21:08:01 +0200

    Test the new test failure mechanism

--- KrameWork/KW_DataContainer.php
+++ KrameWork/KW_DataContainer.php
@@ -7,6 +7,7 @@
 		 */
 		public function __construct($data = array())
 		{
+			if ($source instanceof IDataContainer) return; // Trigger NOTICE in test
 			if ($data instanceof IDataContainer)
 				$this->values = $data->getAsArray();
 			else
