95ef393f5a5a56446c96675b36a5d9dade5575b9
Morten Nilsen <morten@runsafe.no>
2016-03-30 21:12:47 +0200

    Update example to not use remove statics

--- examples/errorCatcher/error.php
+++ examples/errorCatcher/error.php
@@ -1,10 +1,10 @@
 <?php
-	require_once('../../KrameWork/KW_ErrorHandler.php');
-	require_once('../../KrameWork/KW_ErrorReport.php');
-	require_once('../../KrameWork/KW_DatabaseConnection.php');
+	require_once('../../KrameWork/KrameSystem.php');
+	$kernel = new KrameSystem(KW_ERROR_HANDLER);
+	$err = $kernel->getErrorHandler();
 	KW_DatabaseConnection::$trace = true;
-	KW_ErrorHandler::$startup = microtime(true);
-	KW_ErrorHandler::$slowWarn = 0.6;
-	KW_ErrorHandler::$errorDocument = file_get_contents('error.html');
-	ob_start(array('KW_ErrorHandler','errorCatcher'));
+	$err->startup = microtime(true);
+	$err->slowWarn = 0.6;
+	$err->errorDocument = file_get_contents('error.html');
+	ob_start(array($err,'errorCatcher'));
 ?>
