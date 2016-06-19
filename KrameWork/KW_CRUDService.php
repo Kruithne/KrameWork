44cd4acf474a725fa9090a76b74500e9b5896fdb
Morten Nilsen <morten@runsafe.no>
2016-06-19 15:12:47 +0200

    Breaking change: CRUD Service contracts made to match new AutoService, and adding AAA

--- KrameWork/KW_CRUDService.php
+++ KrameWork/KW_CRUDService.php
@@ -98,101 +98,148 @@
 
 		public function process($object)
 		{
-			$path = false;
-			if (isset($_SERVER['PATH_INFO']))
-				$path = $_SERVER['PATH_INFO'];
+			if (!isset($_SERVER['PATH_INFO']))
+				return (object)['success' => false, 'error' => 'Unsupported request'];
+
+			$args = explode('/', $_SERVER['PATH_INFO']);
+			if (count($args) < 2 || !($args[1] == 'create' || $args[1] == 'read' || $args[1] == 'update' || $args[1] == 'delete' || $args[1] == 'query'))
+				return (object)['success' => false, 'error' => 'Unknown method'];
+
+			$method = $args[1];
+			$varargs = count($args) > 2 ? array_slice($args, 2) : [];
+			if ($request !== null)
+				$varargs[] = $request;
 
 			try
 			{
-				if ($_SERVER['REQUEST_METHOD'] == 'POST')
-				{
-					switch($path)
-					{
-						case '/create':
-							if (!$this->canCreate($object))
-							{
-								header('HTTP/1.0 403 Access Denied');
-								return '';
-							}
-							return $this->create($object);
-
-						case '/update':
-							if (!$this->canUpdate($object))
-							{
-								header('HTTP/1.0 403 Access Denied');
-								return false;
-							}
-							$this->update($object);
-							return true;
-
-						case '/delete':
-							if (!$this->canDelete($object))
-							{
-								header('HTTP/1.0 403 Access Denied');
-								return false;
-							}
-							$this->delete($object);
-							return true;
-
-						default:
-							return false;
-					}
-				}
-
-				if ($_SERVER['REQUEST_METHOD'] == 'GET')
-				{
-					if (!$this->canRead())
-					{
-						header('HTTP/1.0 403 Access Denied');
-						return false;
-					}
-
-					if ($path == '/query')
-					{
-						$query = array();
-						$cols = $this->getValues();
-						foreach ($_GET as $k => $v)
-							if (in_array($k, $cols))
-								$query[$k] = $v;
-						$keys = array_keys($query);
-						$q = false;
-						do
-						{
-							$key = array_shift($keys);
-							$q = $q ? $q->andColumn($key) : $this->search($key);
-							if (strpos($query[$key], '%') !== false)
-								$q = $q->like($query[$key]);
-							else
-								$q = $q->equals($query[$key]);
-						}
-						while (count($keys));
-						return $q->execute();
-					}
-					if ($path)
-					{
-						$lookup = explode('/', $path);
-						$key = $this->getKey();
-						if (is_array($key))
-						{
-							$search = array();
-							foreach ($key as $i => $col)
-								$search[$col] = $lookup[$i + 1];
-
-							return $this->read($search);
-						}
-						else
-						{
-							return $this->read($lookup[1]);
-						}
-					}
-					return $this->read();
-				}
+				$return = $this->filter_call($method, $args);
+				if($return === null)
+					$return = call_user_func(array($this, $method), $varargs);
 			}
-			catch(KW_CRUDException $e)
+			catch(Exception $e)
 			{
-				return (object)array('error' => (object)array('type' => 'exception', 'message' => $e->getMessage()));
+				return (object)['success' => false, 'exception' => $e->getMessage()];
 			}
-			return false;
+			return (object)['success' => $return !== false, 'result' => $return === false ? null : $return];
+		}
+
+		/**
+		 * Enable authorization and auditing
+		 * @param string $endpoint The method that will be invoked
+		 * @param string[] $args The arguments to be passed
+		 */
+		public function filter_call($endpoint, $args)
+		{
+			if(!$this->authorize_call($this->user, $endpoint, $args))
+				throw new Exception('Not authorized');
+			$this->audit_call($this->user, $endpoint, $args);
+			return null;
+		}
+
+		/**
+		 * Override this method to implement authorization
+		 * @param IDataContainer $user The calling user
+		 * @param string $endpoint The method being called
+		 * @param string[] $args The arguments given
+		 */
+		public function authorize_call($user, $endpoint, $args)
+		{
+			return true;
+		}
+
+		/**
+		 * Override this method to implement auditing
+		 * @param IDataContainer $user The calling user
+		 * @param string $endpoint The method being called
+		 * @param string[] $args The arguments given
+		 */
+		public function audit_call($user, $endpoint, $args)
+		{
+		}
+
+		public function create($object)
+		{
+			if (!$this->canCreate($object))
+			{
+				header('HTTP/1.0 403 Access Denied');
+				return false;
+			}
+			return parent::create($object);
+		}
+
+		public function update($object)
+		{
+			if (!$this->canUpdate($object))
+			{
+				header('HTTP/1.0 403 Access Denied');
+				return false;
+			}
+			parent::update($object);
+			return true;
+		}
+
+		public function delete($object)
+		{
+			if (!$this->canDelete($object))
+			{
+				header('HTTP/1.0 403 Access Denied');
+				return false;
+			}
+			parent::delete($object);
+			return true;
+		}
+
+		public function query()
+		{
+			if (!$this->canRead())
+			{
+				header('HTTP/1.0 403 Access Denied');
+				return false;
+			}
+			$query = array();
+			$cols = $this->getValues();
+			foreach ($_GET as $k => $v)
+				if (in_array($k, $cols))
+					$query[$k] = $v;
+			$keys = array_keys($query);
+			$q = false;
+			do
+			{
+				$key = array_shift($keys);
+				$q = $q ? $q->andColumn($key) : $this->search($key);
+				if (strpos($query[$key], '%') !== false)
+					$q = $q->like($query[$key]);
+				else
+					$q = $q->equals($query[$key]);
+			}
+			while (count($keys));
+			return $q->execute();
+		}
+
+		public function read($lookup)
+		{
+			if (!$this->canRead())
+			{
+				header('HTTP/1.0 403 Access Denied');
+				return false;
+			}
+			if(count($lookup))
+			{
+				$key = $this->getKey();
+				if (is_array($key))
+				{
+					$search = array();
+					foreach ($key as $i => $col)
+						$search[$col] = $lookup[$i + 1];
+
+					return parent::read($search);
+				}
+				else
+				{
+					return parent::read($lookup[1]);
+				}
+			}
+			return parent::read();
 		}
 
 		protected $user;
