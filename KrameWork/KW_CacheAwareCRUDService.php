dd85c0ee64cae2436a7e2e659de4c9c24cca3f59
Morten Nilsen <morten@runsafe.no>
2016-07-16 20:55:33 +0200

    Fix some bugs related to query api

--- KrameWork/KW_CacheAwareCRUDService.php
+++ KrameWork/KW_CacheAwareCRUDService.php
@@ -214,7 +214,20 @@
 			foreach ($_GET as $k => $v)
 				if (in_array($k, $cols))
 					$query[$k] = $v;
+
+			return $this->readQuery($query);
+		}
+
+		/**
+		 * Override this method to gain fine controlled access restrictions for loading data
+		 * @param array $query key/value pairs passed by the client
+		 * @return mixed An array of matching objects or false
+		 */
+		public function readQuery($query)
+		{
 			$keys = array_keys($query);
+			if(count($keys) == 0)
+				return false;
 			$q = false;
 			do
 			{
@@ -236,7 +249,7 @@
 				header('HTTP/1.0 403 Access Denied');
 				return false;
 			}
-			if(count($lookup))
+			if(is_array($lookup) && count($lookup))
 			{
 				$key = $this->getKey();
 				if (is_array($key))
